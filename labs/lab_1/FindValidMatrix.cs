098db0b93dd0b6e5ae34d193fd654d383574e42e
kodicat <yevhenii.yukhymchuk@gmail.com>
2025-03-10 15:58:18 +0200

    implement lab 1

--- labs/lab_1/FindValidMatrix.cs
+++ labs/lab_1/FindValidMatrix.cs
@@ -1,9 +1,26 @@
 namespace labs.lab_1;
 
-public class FindValidMatrix
+public static class FindValidMatrix
 {
     public static int[][] RestoreMatrix(int[] rowSum, int[] colSum)
     {
-        return new int[rowSum.Length][];
+        rowSum = rowSum.ToArray();
+        colSum = colSum.ToArray();
+
+        var matrix = Enumerable
+            .Range(0, rowSum.Length)
+            .Select(_ => Enumerable.Repeat(0, colSum.Length).ToArray())
+            .ToArray();
+
+        for (var rowIndex = 0; rowIndex < rowSum.Length; rowIndex++)
+        for (var colIndex = 0; colIndex < colSum.Length; colIndex++)
+        {
+            var maxValue = Math.Min(rowSum[rowIndex], colSum[colIndex]);
+            matrix[rowIndex][colIndex] = maxValue;
+            rowSum[rowIndex] -= maxValue;
+            colSum[colIndex] -= maxValue;
+        }
+        
+        return matrix;
     }
 }
--- unit_tests/lab1/Tests.cs
+++ unit_tests/lab1/Tests.cs
@@ -13,20 +13,30 @@ public class Tests
         var matrix = FindValidMatrix.RestoreMatrix(rowSum, colSum);
         TestMatrix(matrix, rowSum, colSum);
     }
+    
+    [Test]
+    public void TestScenarioMoreComplex()
+    {
+        int[] rowSum = [5, 7, 10], colSum = [8, 6, 8];
+        var matrix = FindValidMatrix.RestoreMatrix(rowSum, colSum);
+        TestMatrix(matrix, rowSum, colSum);
+    }
 
     private static void TestMatrix(int[][] matrix, int[] rowSum, int[] colSum)
     {
-        using (var scope = new AssertionScope());
+        using (var _ = new AssertionScope());
         var indexedRows = matrix.Select((row, index) => (index, row));
         foreach (var (index, row) in indexedRows)
         {
             row.Sum().Should().Be(rowSum[index]);
         }
 
-        var indexedCols = Enumerable.Range(0, matrix[0].Length)
-            .Select(colIndex => matrix.Select(row => row[colIndex]).ToArray())
-            .Select((col, index) => (index, col))
-            .ToArray();
+        var indexedCols = 
+            Enumerable
+                .Range(0, matrix[0].Length)
+                .Select(colIndex => matrix.Select(row => row[colIndex]).ToArray())
+                .Select((col, index) => (index, col))
+                .ToArray();
         foreach (var (index, col) in indexedCols)
         {
             col.Sum().Should().Be(colSum[index]);
