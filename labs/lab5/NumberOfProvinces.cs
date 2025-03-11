2ec47782f01ec082b5dbefc34f34b9e12735eded
kodicat <yevhenii.yukhymchuk@gmail.com>
2025-03-12 01:52:26 +0200

    add lab 5 solution

--- labs/lab5/NumberOfProvinces.cs
+++ labs/lab5/NumberOfProvinces.cs
@@ -4,6 +4,34 @@ public static class NumberOfProvinces
 {
     public static int FindCircleNum(int[][] isConnected)
     {
-        return 0;
+        var n = isConnected.Length;
+        var parent = Enumerable.Range(0, n).ToArray();
+
+        for (var i = 0; i < n; i++)
+        for (var j = i + 1; j < n; j++)
+        {
+            if (isConnected[i][j] == 1)
+                Connect(i, j, parent);
+        }
+
+        return parent.Where((x, i) => x == i).Count();
+    }
+
+    private static void Connect(int i, int j, int[] parent)
+    {
+        var iParent = FindParent(parent, i);
+        var jParent = FindParent(parent, j);
+        
+        if (iParent == jParent)
+            return;
+        
+        parent[iParent] = jParent;
+    }
+
+    private static int FindParent(int[] parent, int i)
+    {
+        if (parent[i] == i)
+            return i;
+        return FindParent(parent, parent[i]);
     }
 }
