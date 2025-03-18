e12be2767f3307d2c8f53014ff860ff44ae36e2b
kodicat <yevhenii.yukhymchuk@gmail.com>
2025-03-18 11:20:17 +0200

    add lab 9 solution

--- labs/lab9/IntegerToRoman.cs
+++ labs/lab9/IntegerToRoman.cs
@@ -1,9 +1,37 @@
+using System.Text;
+
 namespace labs.lab9;
 
 public static class IntegerToRoman
 {
+    private static readonly (int, string)[] Translations =
+    [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I"),
+    ];
+
     public static string IntToRoman(int num)
     {
-        return "";
+        var romanNumberBuilder = new StringBuilder();
+        foreach(var (arabic, roman) in Translations)
+        {
+            while (num >= arabic)
+            {
+                romanNumberBuilder.Append(roman);
+                num -= arabic;
+            }
+        }
+        return romanNumberBuilder.ToString();
     }
 }
