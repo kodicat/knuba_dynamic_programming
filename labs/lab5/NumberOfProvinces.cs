b4ed91e7fe04422d7e891a4dedfbe541aa471c1d
kodicat <yevhenii.yukhymchuk@gmail.com>
2025-03-21 12:48:04 +0200

    update lab 5: add compression and ranking

--- labs/lab5/NumberOfProvinces.cs
+++ labs/lab5/NumberOfProvinces.cs
@@ -7,32 +7,48 @@ public static class NumberOfProvinces
     {
         var n = isConnected.Length;
         var parent = Enumerable.Range(0, n).ToArray();
+        var rank = new int[n];
 
         for (var i = 0; i < n; i++)
         for (var j = i + 1; j < n; j++)
         {
             if (isConnected[i][j] == 1)
-                Connect(i, j, parent);
+                Union(i, j, parent, rank);
         }
 
         return parent.Where((x, i) => x == i).Count();
     }
 
-    private static void Connect(int i, int j, int[] parent)
+    private static void Union(int i, int j, int[] parent, int[] rank)
     {
         var iParent = FindParent(parent, i);
         var jParent = FindParent(parent, j);
         
         if (iParent == jParent)
             return;
-        
+
+        if (rank[iParent] > rank[jParent])
+        {
+            parent[jParent] = iParent;
+        }
+        else if (rank[iParent] < rank[jParent])
+        {
+            parent[iParent] = jParent;
+        }
+        else
+        {
+            rank[jParent] = iParent;
+            rank[iParent]++;
+        }
+
         parent[iParent] = jParent;
     }
 
     private static int FindParent(int[] parent, int i)
     {
-        if (parent[i] == i)
-            return i;
-        return FindParent(parent, parent[i]);
+        if (parent[i] != i)
+            parent[i] = FindParent(parent, parent[i]);
+
+        return parent[i];
     }
 }
